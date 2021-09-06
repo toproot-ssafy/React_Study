69d6c0a9752b6d6b38afc16933c4850d68067086
toproot <toproot7913@gmail.com>
2021-09-06 17:08:42 +0900

    react router - dynamic router

---
+++ react-router-example/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import queryString from "query-string";
+
+export default function About(props) {
+  console.log(props);
+  const searchParams = props.location.search;
+  console.log(searchParams);
+  // const obj = new URLSearchParams(searchParams);
+  // console.log(obj.get("name"));
+  const query = queryString.parse(searchParams);
+  console.log(query);
+  return (
+    <div>
+      <h2>About 페이지입니다.</h2>
+      {query.name && <p>name은 {query.name} 입니다. </p>}
+    </div>
+  );
+}
